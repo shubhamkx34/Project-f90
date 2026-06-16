88222f3ae2c652a4218bde45463bd8eeb0784c1f
Shubham <shubhamkx34@gmail.com>
2026-06-17 01:20:48 +0530

    Added fixed image cards using GSAP animations in machine page.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,13 @@ import Records from "./pages/records";
 const App = () => {
   return (
     <div className="w-screen min-h-screen text-4xl bg-black text-white">
+      
+    <div className="h-screen w-full fixed z-1">
+      <div className="h-full w-1/5 bg-black "></div>
+    </div>
+      
+      
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/machine" element={<Machine />} />
--- src/pages/machine.jsx
+++ src/pages/machine.jsx
@@ -1,18 +1,64 @@
-import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import React, { useRef, useEffect } from "react";
 
 const Machine = () => {
+  gsap.registerPlugin(ScrollTrigger);
+  const imageDivRef = useRef(null);
+  const imgRef = useRef(null);
+  const imgarray = [
+    "/img1.jpg",
+    "/img2.jpg",
+    "/img3.jpg",
+    "/img4.jpg",
+    "/img5.jpg",
+    "/img6.jpg",
+    "/img7.jpg",
+    "/img8.jpg",
+    "/img9.jpg",
+  ];
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  useGSAP(() => {
+    gsap.to(imageDivRef.current, {
+      scrollTrigger: {
+        trigger: imageDivRef.current,
+        start: "top 25.8%",
+        end: "top -160%",
+        scrub: 1,
+        pin:true,
+        onUpdate:(elem) =>{                                //Image scrolling with gsap animations
+          const imgindex = 
+           Math.min( Math.floor(elem.progress * imgarray.length), imgarray.length - 1, );  
+           //Math.min( [Whatever number your scroll calculates] , 8 )
+          imgRef.current.src = imgarray[imgindex]
+        }
+      },
+    });
+  });
+
+
   return (
+
     <div className="bg-white min-h-screen w-full flex flex-col overflow-x-hidden ">
-      <div className="absolute rounded-full top-[13vw] left-[37vw] h-[21vw] w-[21vw] overflow-hidden ">
-        <img className="h-full w-full object-cover" src="/img8.jpg" alt="" />
+      <div
+        ref={imageDivRef}
+        className="  absolute  rounded-full top-[12vw] left-[38vw] h-[22vw] w-[22vw] overflow-hidden "
+      >
+        <img
+          ref={imgRef}
+          className="h-full w-full object-cover"
+        />
       </div>
-      <div>
-        <div className=" relative mt-[55vh] w-full flex justify-center">
+      <div className="relative ">
+        <div className="mt-[55vh] w-full flex justify-center">
           <h1 className="text-[20vw] text-black font-[font2] leading-[17vw] uppercase text-center">
             Six-<br></br>Thirty<br></br>five
           </h1>
         </div>
-        <div className="text-right mt-2 pr-19 mb-2 ">
+        <div className="text-right mt-2 pr-19 mb-2 h-[85vw] ">
           <p className="text-black text-[3.5vw] font-semibold leading-[3.8vw] tracking-tighter">
             Our racing heritage <br />
             nourishes our <span className="text-red-800">performance</span> . We{" "}
